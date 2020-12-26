96dcc2406fc156ea5525d419dd91ba28dff39173
Casper Hauberg <casperhauberg@icloud.com>
2020-12-26 14:12:53 +0100

    38

--- lib/main.dart
+++ lib/main.dart
@@ -7,18 +7,18 @@ void main() {
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return MyAppState();
+    return _MyAppState();
   }
 }
 
-class MyAppState extends State<MyApp> {
-  var questionIndex = 0;
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
 
-  void answerQuestion() {
+  void _answerQuestion() {
     setState(() {
-      questionIndex += 1;
+      _questionIndex += 1;
     });
-    print(questionIndex);
+    print(_questionIndex);
   }
 
   @override
@@ -33,10 +33,10 @@ class MyAppState extends State<MyApp> {
         title: Text('My First App'),
       ),
       body: Column(children: [
-        Text(questions[questionIndex]),
+        Text(questions[_questionIndex]),
         RaisedButton(
           child: Text('Answer 1'),
-          onPressed: answerQuestion,
+          onPressed: _answerQuestion,
         ),
         RaisedButton(
           child: Text('Answer 2'),
